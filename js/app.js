3fa334e364959e1802cc82ec3550ff764ebb3af6
Carlos Valverde <cevc1184@gmail.com>
2020-03-27 23:56:21 -0600

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+"use strict"/**
+* @name app.js
+* @file Add a small description for this file.
+* @author Add Your Name Here <addyouremail@mail.com>
+* @version 1.0.0
+*/
+
+window.addEventListener('load', init, false);
+
+function init() {
+	console.log('App running!');
+	//1. Declare variables
+	//2. Initialize variables
+	//3. Events
+	//4. Program Logic
+}
